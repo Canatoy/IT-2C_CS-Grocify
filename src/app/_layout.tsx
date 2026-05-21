dcd52642bc3725fa00946fe0a0c0854515501a84
Zyxary <kelpselp@gmail.com>
2026-05-21 22:58:03 +0800

    feat: integrate Sentry for error tracking and update dependencies

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,10 @@ import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import * as Sentry from '@sentry/react-native';
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
+
 
 // @ts-ignore:
 import "../../global.css";
@@ -15,7 +17,12 @@ if (!publishableKey) {
 }
 
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://6bdc1ead45e3dbbd5a46c989c07052e8@o4511428416241664.ingest.us.sentry.io/4511428417748992',
+  integrations: [Sentry.feedbackIntegration()],
+});
+
+export default Sentry.wrap (function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
@@ -26,4 +33,4 @@ export default function RootLayout() {
 
     </ClerkProvider>
   );
-}
+});
